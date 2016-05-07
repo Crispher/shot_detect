385086bea9cb98876e2ca8c2216459d0b25d2609
Crispher <guyu13@mails.tsinghua.edu.cn>
2016-05-07 16:43:17 +0800

    Updated py and readme

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 Shot boundary detection
-
     Use histograms to detect shots of a video.
+    Usage:
+    >>> python shot_detect.py <video_path> <output_directory>
+    The starting frame of each shot is stored to output_dir.
+    
+    Adjust sensitivity: tune the parameter thres(=1.5 by default) in range(0, 3). The higher the more sensitive.
     
     Environment:
-    - Open CV 3.1.1, Python 3.5 binding.
-    - Windows platform
-     
+    - Open CV 3.1.0, Python 3.5 binding.
+
+This project extends the codes on github by @yu239.
--- shot_detect.py
+++ shot_detect.py
@@ -4,15 +4,14 @@ import os
 import sys
 from scipy.spatial.distance import cityblock
 
-__hist_size__ = 64             # how many bins for each R,G,B histogram
-__min_duration__ = 1           # if a shot has length less than this, merge it with others
-__absolute_threshold__ = 0.8   # any transition must be no less than this threshold
-
-class shotDetector: 
-    def __init__(self, video_path=None, min_duration=__min_duration__, output_dir=None): 
-        self.video_path = video_path 
+class shot_detector: 
+    def __init__(self, video_path=None, min_duration=10, output_dir=None, thres = 1.5): 
+        self.video_path = video_path
         self.min_duration = min_duration 
-        self.output_dir = output_dir 
+        self.output_dir = output_dir
+        self.hist_size = 64             # how many bins for each R,G,B histogram
+        self.absolute_threshold = thres # any transition must be no less than this threshold range from 0 to 3, the higher the more sensitive.
+ 
     def run(self, video_path=None):
         if video_path is not None:
             self.video_path = video_path    
@@ -29,27 +28,22 @@ class shotDetector:
             if self.output_dir is not None:
                 frames.append(frame)
             # compute RGB histogram for each frame
-            color_histgrams = [cv2.calcHist([frame], [c], None, [__hist_size__], [0,256]) \
+            color_histgrams = [cv2.calcHist([frame], [c], None, [self.hist_size], [0,256]) \
                           for c in range(3)]
                           
             color_histgrams = np.array([chist/float(sum(chist)) for chist in color_histgrams])
             
             hists.append(color_histgrams.flatten())
 
-        # compute hist chisquare distances
-
-        # scores = [cv2.compareHist(pair[0],pair[1],method=cv2.HISTCMP_CHISQR) \
-        #               for pair in zip(hists[1:], hists[:-1])]
-
-                      
+        # manhattan distance of two consecutive histgrams
         scores = [cityblock(*h12) for h12 in zip(hists[:-1], hists[1:])]
-        print(scores)
+        
         print("max diff:", max(scores), "min diff:", min(scores))
         
         # compute automatic threshold
         mean_score = np.mean(scores)
         std_score = np.std(scores)
-        threshold = __absolute_threshold__
+        threshold = self.absolute_threshold
 
         # decide shot boundaries
         prev_i = 0
@@ -63,7 +57,7 @@ class shotDetector:
         self.shots.append((prev_i, video_length))
         assert video_length>=self.min_duration, "duration error"
 
-        # self.merge_short_shots()
+        self.merge_short_shots()
         
         # save key frames
         if self.output_dir is not None:
@@ -102,11 +96,11 @@ class shotDetector:
                 self.shots = left_half + [(shot[0],right_half[0][1])] + right_half[1:]
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print("usage: ./shotdetect.py <video-path> [<key-frames-output-dir>]")
+    if len(sys.argv) != 3:
+        print("usage: ./shotdetect.py <video-path> <output_dir>")
         sys.exit()
     video_path = sys.argv[1]
-    key_frames_dir = None if len(sys.argv)<3 else sys.argv[2]
-    detector = shotDetector(video_path, output_dir=key_frames_dir)
+    key_frames_dir = sys.argv[2]
+    detector = shot_detector(video_path, output_dir=key_frames_dir, thres=1.5)
     detector.run()
     print(detector.shots)
